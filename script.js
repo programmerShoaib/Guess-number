f64eba82b4a00ba73310e60535f9669dc409ff2f
programmerShoaib <link2shoaib.2002@gmail.com>
2025-02-08 22:59:25 +0500

    edit my files

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let guess = document.getElementById("guess");
 let submit = document.getElementById("submit");
 let result = document.getElementById("result");
 let random = Math.floor(Math.random() * 10) + 1;
+let attempts = document.getElementById("attempts");
 submit.addEventListener("click", () => {
     if (guess.value == random) {
         result.innerHTML = "You won";
     } else {
         result.innerHTML = "You lost";
     }
+    attempts.innerHTML = Number(attempts.innerHTML) + 1;
 })
